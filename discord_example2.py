655d16cb4b27bedc5c7b63892f15118f132da662
epfvlssm <124798971+epfvlssm@users.noreply.github.com>
2023-02-11 00:00:04 +0900

    Add files via upload

---
+++ discord_example2.py
@@ -0,0 +1,43 @@
+import random
+import discord
+
+TOKEN = 'MTA3MjgwOTA3NDkwMDYwMjk4MA.GgkIqk.JUUKWtA8DTLk2cOD0EPnbuwMHbqrOM-LeTvCN0'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents) # client 생성. 디스코드와 연결
+
+# 콜백 스타일: 콜백은 기본적으로는 무엇인가 일어났을때 호출되는 기능
+@client.event # 데코레이터 - 이벤트 등록
+async def on_ready(): # 봇이 로깅을 끝내고 여러가지를 준비한 뒤 호출
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message): # 봇이 메시지를 받았을 때 호출됩니다
+    
+    if message.author == client.user: # 봇이 보낸 메세지면 무시
+        return
+
+    if message.content.startswith('#hello'): # 메세지가 #hello로 시작하는 경우 
+        await message.channel.send(f'{message.author.mention}, hello!') # 답장 x
+    elif message.content.startswith('#안녕하세요'): # 메세지가 #안녕하세요 로 시작하는 경우
+        await message.channel.send(f'{message.author.display_name}, 안녕하세요!', reference=message) # 답장
+    elif message.content.startswith('#다이스'): # 메세지가 dice로 시작하는 경우
+        dice_result = str(random.randint(1,100)) # 1~100 랜덤 선택 (1d100)
+        await message.channel.send(f'다이스를 굴리자... <{dice_result}>이 나왔다.', reference=message) # 답장 o
+    elif message.content.startswith('❈가챠'): # 메세지가 ❈가챠 로 시작하는 경우
+        dice_result = str(random.choice(['모르는 이름이 써진 네임택', '티 타임용 쿠키세트', '작은 씨앗', '챙 넓은 모자', '고래 인형', '워터볼', '검은 강아지 인형', '동화책 [어느 숲속 이야기]', '텔 원석', '건네 준 사람과의 추억이 재생되는 거울 모양 마도구', '민들레 홀씨', '새장', '모조 다이아', '새하얀 천', '금박장식 비녀', '고사성어 사전', '별자리 무드등', '「쉽게 배우는 마공학 이론」', '새하얀 양 인형', '실타래', 'SEI☆의 사인지', '무선마이크', '목이 기다란 도마뱀 인형', '공룡이 그려진 동화책', '낡은 누더기 옷', '행운 기원 팔찌', '기념품가게 팔찌', '향수', '화상 연고', '의료용 안대', '목화 다발', '자개 장식', '공단 리본', '레이스가 달린 원피스', '물감', '긴 천', '편지지 세트', '알록달록한 펜', '애벌레 인형', '발냄새 나는 양말'])) # 1~100 랜덤 선택 (1d100)
+        await message.channel.send(f'가챠를 굴리자... <{dice_result}>이 나왔다.', reference=message) # 답장 o    
+    else:
+        start = message.content.find('[')
+        end = message.content.find(']')
+        if (start != -1 and end != -1) and start<end: # [] 조건 찾기. [, ]가 존재해야 하고, 닫는 괄호가 여는 괄호보다 앞에 있으면 안된다.
+            mention_keyword = message.content[start+1:end].strip().split('/') # /를 기준으로 나눠 리스트로 저장. 현재 받은 메세지에는 /가 없으므로 그냥 ['다이스'] 로 저장된다. 
+            first_keyword = mention_keyword[0].strip()
+            if first_keyword == '다이스':
+                dice_result = str(random.randint(1,100))
+                await message.channel.send(f'다이스를 굴리자... <{dice_result}>이 나왔다.', reference=message) # 답장 o
+    
+
+client.run(TOKEN)
